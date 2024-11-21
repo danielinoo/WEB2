c94b6161b3c8151927710a308902339826509d8e
danielinoo <daniele.pietropaolo1234@gmail.com>
2024-11-21 13:13:11 +0100

    React

---
+++ React/src/componenti/Navbar.jsx
@@ -0,0 +1,12 @@
+function Navbar() {
+    return(
+        <ul>
+            <li><a href="#">Ciao</a></li>
+            <li><a href="#">Ciao</a></li>
+            <li><a href="#">Ciao</a></li>
+
+        </ul>
+
+    )
+}
+export default Navbar
