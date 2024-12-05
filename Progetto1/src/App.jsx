99cca46572c9227c7e39c097152a5dc20c60849c
danielinoo <daniele.pietropaolo1234@gmail.com>
2024-12-05 13:50:09 +0100

    progetto1

--- Progetto1/src/App.jsx
+++ Progetto1/src/App.jsx
@@ -6,12 +6,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './componenti/Card'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Card vis={true}  titolo="TROPEA" descrizione = "descrizione foto" img = "https://www.calabriago.com/wp-content/uploads/2020/06/slide-1.jpg" ></Card>
+      <Card vis= {true}  titolo="MAIAMI" descrizione = "descrizione foto" img= "https://plus.unsplash.com/premium_photo-1697730215093-baeae8060bfe?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bWlhbWl8ZW58MHx8MHx8fDA%3D"></Card>
+      <Card  vis ={true}  titolo="ZANTE" descrizione = "descrizione foto" img = "https://www.grecia.info/wp-content/uploads/sites/71/zante.jpg"></Card>
+      <Card  vis ={true}  titolo="PUNTA CANA" descrizione = "descrizione foto" img="https://d2lxis1uiqe6st.cloudfront.net/wp-content/uploads/2023/08/puntacana.png"></Card>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ Progetto1/src/componenti/Card.jsx
@@ -0,0 +1,100 @@
+import './Card.css'
+
+//************PRIMO METODO PER PASSARE LE VARIABILI******* */
+
+// function Card(props){
+//     const img = props.img;
+//     const descrizione = props.descrizione;
+//     const titolo = props.titolo;
+
+
+
+
+
+//******************SECONDO METODO PER PASSARE LE VARIABILI************** */
+
+// // eslint-disable-next-line react/prop-types
+// function Card({descrizione,titolo,img}){
+
+//     return(
+//         <div>            
+//             <h2>{titolo}</h2>
+//             <p>{descrizione}</p>
+//             <img className= "robaimmagini" src = {img} ></img>
+
+            
+//         </div>
+
+
+//     )
+
+// }
+// export default Card
+
+
+
+//************************PRIMO METODO IF  ELSE*********************** */
+
+// // eslint-disable-next-line react/prop-types
+// function Card({descrizione,titolo,img,vis}){
+
+//     if (vis){
+
+//     return(
+//         <div>            
+//             <h2>{titolo}</h2>
+//             <p>{descrizione}</p>
+//             <p>Visitato</p>
+//             <img className= "robaimmagini" src = {img} ></img>
+
+
+            
+//         </div>
+
+
+//     )
+//     } else{
+//         return(
+//             <div>            
+//                 <h2>{titolo}</h2>
+//                 <p>{descrizione}</p>
+//                 <p>Non visitato</p>
+//                 <img className= "robaimmagini" src = {img} ></img>
+    
+    
+                
+//             </div>
+//         )
+    
+//     }
+
+// }
+// export default Card
+
+
+
+//*********************************SECONDO METODO IF ELSE*******************
+// {vis ? <span>visitato</span> : <span>Non visitato</span>} if else
+//  {vis && <span>visitato</span>}     vis è vero allora te lo faccio vede senno no
+
+
+// eslint-disable-next-line react/prop-types
+function Card({descrizione,titolo,img,vis}){
+
+    return(
+        <div>            
+            <h2>{titolo}</h2>
+            <p>{descrizione}</p>
+            {vis ? <span><strong>visitato</strong></span> : <span><strong>Non visitato</strong></span>}
+            <br></br> <br></br>
+            <img className= "robaimmagini" src = {img} ></img>
+            <br></br><br></br>
+            <hr className='hrm'></hr>
+            
+        </div>
+
+
+    )
+
+}
+export default Card
